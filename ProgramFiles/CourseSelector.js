d017246572b96f674d946a0f5578ab9fd1a5524b
EmmaB3 <30680691+EmmaB3@users.noreply.github.com>
2018-07-28 22:04:18 -0700

    ugh

---
+++ ProgramFiles/CourseSelector.js
@@ -0,0 +1,376 @@
+﻿var boxes1 = [];
+var boxes2 = [];
+var checkedBoxes = [];
+var selectedCourses = [];
+var page = 0;
+var pageElements = [[]];
+var offBlockNum;
+var blockArrays = [];
+var schedules = [];
+var error = false;
+
+function courseCheckboxes() {
+    for (var a = 0; a < deptTitles.length; a++) {
+        var header = document.createElement("h3");
+        header.innerHTML = deptTitles[a];
+        pageElements[0].push(header);
+        //document.body.appendChild(header);
+        document.getElementById("mainStuff").appendChild(header);
+        for (var b = 0; b < allCourses.length; b++) {
+            console.log("hey");
+            console.log(allCourses[b].dept);
+            if (allCourses[b].dept == a + 1) {
+                var cb = document.createElement("input");
+                cb.type = "checkbox";
+                cb.id = allCourses[b].title;
+                //cb.class = "page0";
+                pageElements[0].push(cb);
+                //document.body.appendChild(cb);
+                document.getElementById("mainStuff").appendChild(cb);
+                boxes1.push(cb);
+                var label = document.createElement("label");
+                label.for = allCourses[b].title;
+                label.innerHTML = allCourses[b].title;
+                //label.class = "page0";
+                pageElements[0].push(label);
+                //document.body.appendChild(label);
+                document.getElementById("mainStuff").appendChild(label);
+                var br = document.createElement("br");
+                //br.class = "page0";
+                pageElements[0].push(br);
+                //document.body.appendChild(br);
+                document.getElementById("mainStuff").appendChild(br);
+            }
+        }
+    }
+}
+
+function getSelectedCourses() {
+    checkedBoxes = [];
+    selectedCourses = [];
+    error = false;
+    for (var a = 0; a < boxes1.length; a++) {
+        if (boxes1[a].checked) {
+            checkedBoxes.push(boxes1[a]);
+        }
+    }
+    /*if (checkedBoxes.length > 7) {
+        error = true;
+        errorCode("It seems like you've picked too many classes.");
+    }*/
+    //TODO:FIGURE OUT WHY IT'S BREAKING THE TEACHERS PAGE and also why it still changes page if you click next page again without changing anything
+    //TODO: try again button at end of schedules
+    //TODO: error code if schedules is empty after findGoodSchedules
+    //TODO: maybe add in more complex error codes later (like what u were working on in beginning of generateschedules)
+    for (var a = 0; a < allCourses.length; a++) {
+        for (var b = 0; b < checkedBoxes.length; b++) {
+            if (allCourses[a].title == checkedBoxes[b].id) {
+                selectedCourses.push(allCourses[a]);
+            }
+        }
+    }
+    boxes1 = [];
+}
+
+function teacherCheckboxes() {
+    for (var a = 0; a < selectedCourses.length; a++) {
+        var header = document.createElement("h3");
+        header.innerHTML = selectedCourses[a].title;
+        //document.body.appendChild(header);
+        document.getElementById("mainStuff").appendChild(header);
+        pageElements[1].push(header);
+        for (var b = 0; b < selectedCourses[a].teacherNames.length; b++) {
+            var cb = document.createElement("input");
+            cb.type = "checkbox";
+            cb.id = selectedCourses[a].teacherNames[b];
+            cb.className = selectedCourses[a].title;
+            //cb.class = "page1";
+            //document.body.appendChild(cb);
+            document.getElementById("mainStuff").appendChild(cb);
+            boxes2.push(cb);
+            pageElements[1].push(cb);
+            var label = document.createElement("label");
+            label.for = selectedCourses[a].teacherNames[b];
+            label.innerHTML = selectedCourses[a].teacherNames[b];
+            pageElements[1].push(label);
+            //label.class = "page1";
+            //document.body.appendChild(label);
+            document.getElementById("mainStuff").appendChild(label);
+            var br = document.createElement("br");
+            //br.class = "page1";
+            pageElements[1].push(br);
+            //document.body.appendChild(br);
+            document.getElementById("mainStuff").appendChild(br);
+        }
+    }
+}
+
+function nextPage() {
+    switch (page) {
+        case 0:
+            pageElements.push([]);
+            getSelectedCourses();
+            if (!error) {
+                document.getElementById("header").innerHTML = "Choose Your Teachers";
+                teacherCheckboxes();
+                checkedBoxes = [];
+                removeStuff(page);
+                page++;
+            }
+            break;
+        case 1:
+            pageElements.push([]);
+            getSelectedTeacherBlocks();
+            removeStuff(page);
+            document.getElementById("header").innerHTML = "Choose Your Off Blocks";
+            var instruct = document.createElement("p");
+            instruct.innerHTML = "Pick as many options for each off block as you see fit";
+            document.getElementById("mainStuff").appendChild(instruct);
+            pageElements[2].push(instruct);
+            offCheckboxes();
+            page++;
+            break;
+        case 2:
+            document.getElementById("header").innerHTML = "Your Schedules:";
+            makeOffBlocks();
+            makeBlockArrays();
+            generateSchedules();
+            findGoodSchedules();
+            displaySchedules();
+            document.body.removeChild(document.getElementById("nextButton"));
+            //tryAgainButton();
+            removeStuff(page);
+            break;
+    }
+}
+
+function makeBlockArrays() {
+    for (var a = 1; a <= selectedCourses.length; a++) {
+        var arr = [];
+        for (var b = 0; b < selectedCourses.length; b++) {
+            var arr2 = selectedCourses[b].getBlocks();
+            if (inArray(a, arr2)) {
+                arr.push(selectedCourses[b]);
+            }
+        }
+        blockArrays.push(arr);
+    }
+}
+
+function errorCode(message) {
+    var eHeader = document.createElement("h2");
+    eHeader.innerHTML = "Error";
+    //eHeader.className = "error";
+    document.getElementById("error").appendChild(eHeader);
+    pageElements[page].push(eHeader);
+    var eMessage = document.createElement("p");
+    eMessage.innerHTML = message;
+    //eMessage.className = "error";
+    document.getElementById("error").appendChild(eMessage);
+    pageElements[page].push(eMessage);
+}
+
+function tryAgainButton() {
+    document.getElementById("buttonItself").innerHTML = "Try Again";
+    document.getElementById("buttonItself").onClick = "Try Again";
+    window.location.href = 'course-selector.html'; 
+}
+
+function findGoodSchedules() {
+    //deletes duplicate schedules (can happen w/ off blocks) WAIT BUT THIS ISN'T BASED OFF TITLES SO IT DOESN'T MATTER UGH GOTTA DO THAT ANOTHER WAY I GUESS
+    /*for (var a = 0; a < schedules.length; a++) {
+        for (var b = schedules.length - a; b >= 1; b--) {
+            if (schedules[a] == schedules[a + b]) {
+                schedules.splice(a + b, 1);
+            }
+        }
+    }*/
+    //deletes generated schedules that have duplicate classes inside them
+    for (var a = schedules.length - 1; a >= 0; a--) {
+        var nope = false;
+        for (var b = 0; b < schedules[a].length; b++) {
+            for (var c = 0; c < schedules[a].length - b; c++) {
+                if (schedules[a][b] == schedules[a][b + c] && b != b + c) {
+                    nope = true;
+                }
+            }
+        }
+        if (nope) {
+            schedules.splice(a, 1);
+        }
+    }
+}
+
+function displaySchedules() {
+    for (var a = 0; a < schedules.length; a++) {
+        makeHTML("h3", "Schedule " + (a + 1) + ":");
+        for (var b = 0; b < schedules[a].length; b++) {
+            var teachersArr = schedules[a][b].getTeachersForBlock(b + 1);
+            if (teachersArr.length == 1) {
+                teachersList = teachersArr[0];
+            } else {
+                for (var c = 0; c < teachersArr.length; c++) {
+                    teachersList += c == teachersArr.length - 1? teachersArr[c] : teachersArr[c] + " or ";
+                }
+            }
+            makeHTML("p", (b + 1) + ": " + schedules[a][b].title + (schedules[a][b].title == "OFF"? "" : " - " + teachersList));
+        }
+    }
+}
+//TODO maybe instead of getting rid of next button make it be a try again or whatever button and it reloads the page?? (like just calls up the html page over again??)
+function makeHTML(type, words) {
+    var elem = document.createElement(type);
+    elem.innerHTML = words;
+    document.getElementById("mainStuff").appendChild(elem);
+}
+//TODO put either in here or in makeBlockArrays thing that checks if a class only has one block option and if so removes other classes from that block
+function generateSchedules() {
+    //UNTESTED SO IT'S PROBABLY A BUGGY MESS PLS CHECK ALL SYNTAX
+    //ya know what we're just gonna save this for later
+    /*for (var a = 0; a < 8; a++) {
+        var singles = [];
+        var zeroes = [];
+        if (blockArrays[a].length == 0) {
+            zeroes.push(blockArrays[a]);
+        } else if (blockArrays[a].length > 1) {
+            for (var b in blockArrays[a]) {
+                if (blockArrays[a][b].getBlocks().length == 1) {
+                    singles.push(blockArrays[a][b]);
+                }
+            }
+        }
+        if (singles.length == 1) {
+            blockArrays[a] = [singles[0]];
+        } else if (singles.length > 1) {
+            var message = "It appears your courses ";
+            for (var b in singles) {
+                if (b == singles.length - 1) {
+                    message += "and ";
+                }
+                message += singles[b].title();
+                if (b != singles.length - 1 && singles.length > 2) {
+                    message += ","
+                }
+                message += " ";
+            }
+            message += "are not compatible. Try choosing a wider range of teacher options, or talk to your counselor about dropping one of these courses.";
+            errorCode(message);
+            break;
+        }
+        if (zeroes.length >= 1) {
+            var message = "It appears " + (zeroes.length == 1 ? "block " : "blocks ");
+            for (var b in zeroes) {
+                if (singles.length > 1 && b == singles.length - 1) {
+                    message += " and";
+                } 
+                messsage += singles[b].title;
+            }
+            message += (zeroes.length == 1 ? " doesn't" : " don't") + " have any possible classes to fill  " + (zeroes.length == 1 ? "it " : "them ") + ". Try choosing a broader range of teachers, or putting " + (zeroes.length == 1 ? "this block as an option " : "these blocks as options ") + "for off blocks.";
+            break;
+        } 
+    }*/
+    for (var a in blockArrays[0]) {
+        for (var b in blockArrays[1]) {
+            for (var c in blockArrays[2]) {
+                for (var d in blockArrays[3]) {
+                    for (var e in blockArrays[4]) {
+                        for (var f in blockArrays[5]) {
+                            for (var g in blockArrays[6]) {
+                                for (var h in blockArrays[7]) {
+                                    var currentSchedule = [blockArrays[0][a], blockArrays[1][b], blockArrays[2][c], blockArrays[3][d], blockArrays[4][e], blockArrays[5][f], blockArrays[6][g], blockArrays[7][h]];
+                                    schedules.push(currentSchedule);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+function offCheckboxes() {
+    offBlockNum = 8 - selectedCourses.length;
+    for (var a = 1; a <= offBlockNum; a++) {
+        var header = document.createElement("h3");
+        header.innerHTML = "Off Block #" +  a;
+        document.getElementById("mainStuff").appendChild(header);
+        pageElements[2].push(header);
+        for (var b = 1; b <= 8; b++) {
+            var cb = document.createElement("input");
+            cb.type = "checkbox";
+            cb.id = "blk" + b;
+            cb.className = "num" + a;
+            document.getElementById("mainStuff").appendChild(cb);
+            boxes1.push(cb);
+            pageElements[2].push(cb);
+            var label = document.createElement("label");
+            label.for = "blk" + b;
+            label.innerHTML = "Block " + b;
+            pageElements[2].push(label);
+            document.getElementById("mainStuff").appendChild(label);
+            var br = document.createElement("br");
+            pageElements[2].push(br);
+            document.getElementById("mainStuff").appendChild(br);
+        }
+    }
+}
+
+function makeOffBlocks() {
+    var blockChoices = [];
+    for (var a = 1; a <= offBlockNum; a++) {
+        var singleBlock = [];
+        for (var b = 0; b < 8; b++) {
+            if (boxes1[b].checked){
+                singleBlock.push(b + 1);
+            }
+        }
+        blockChoices.push(singleBlock);
+    }
+    
+    for (var a = 0; a < offBlockNum; a++) {
+        let ob = new Course("OFF", 9, ["N/A"], [blockChoices[a]]);
+        selectedCourses.push(ob);
+    }
+}
+
+function getSelectedTeacherBlocks() {
+    for (var a = 0; a < boxes2.length; a++) {
+        if (boxes2[a].checked) {
+            checkedBoxes.push(boxes2[a]);
+        }
+    }
+ 
+    for (var a = 0; a < selectedCourses.length; a++) {
+        var teacherNameIndexes = [];
+        for (var b = 0; b < selectedCourses[a].teacherNames.length; b++) {
+            for (var c = 0; c < checkedBoxes.length; c++) {
+                if (checkedBoxes[c].className == selectedCourses[a].title && checkedBoxes[c].id == selectedCourses[a].teacherNames[b]) {
+                    teacherNameIndexes.push(b);
+                }
+            }
+        }
+
+        for (var b = selectedCourses[a].teacherNames.length - 1; b >= 0; b--) {
+            if (!inArray(b, teacherNameIndexes)) {
+                selectedCourses[a].teacherNames.splice(b,1);
+                selectedCourses[a].teacherBlocks.splice(b,1);
+            }
+        }
+    }
+} 
+
+function inArray(obj, arr) {
+    for (var a in arr) {
+        if (arr[a] == obj) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function removeStuff(page) {
+    for (var a = 0; a < pageElements[page].length; a++) {
+        document.getElementById(pageElements[page][a].parentElement.id).removeChild(pageElements[page][a]);
+    }
+}
---
+++ ProgramFiles/storeroom.js
@@ -0,0 +1,130 @@
+﻿var allCourses = [];
+var deptTitles = ["Math", "Science", "English", "History", "VPA", "World Language", "PE", "Other"];
+var currentClass = "none";
+var rawData;
+
+class Course {
+    constructor(title, dept, teacherNames, teacherBlocks) {
+        this.title = title;
+        this.dept = dept;
+        this.teacherNames = teacherNames;
+        this.teacherBlocks = teacherBlocks;
+        allCourses.push(this);
+    }
+
+    getBlocks() {
+        var blocks = [];
+        for (var a = 0; a < this.teacherNames.length; a++) {
+            blocks = blocks.concat(this.teacherBlocks[a]);
+        }
+        return blocks;
+    }
+
+    getTeachersForBlock(block) {
+        var teachers = [];
+        for (var a = 0; a < this.teacherBlocks.length; a++) {
+            if (inArray(block, this.teacherBlocks[a])) {
+                teachers.push(this.teacherNames[a]);
+            }
+        }
+        return teachers;
+    }
+}
+
+var stuff = Tabletop.init({
+    key: '1v85ZUS42eJ2fh_kCk3VGzlp3HBWwdvN5C6hYfg3WvWE',
+    callback: processData,
+    simpleSheet: true
+})
+
+function showInfo(data, tabletop) {
+    alert('Successfully processed!')
+    //rawData = (stuff.sheets("Sheet1").toArray());
+    processData();
+}
+
+function processData() {
+    rawData = stuff.sheets("Sheet1").toArray();
+    while (rawData.length > 0) {
+        if (currentClass == "none") {
+            currentClass = rawData[0][2];
+        } else {
+            var pertainingRows = [];
+            var teacherNames = [];
+            var teacherBlocks = [];
+            for (var a in rawData) {
+                if (rawData[a][2] == currentClass) {
+                    pertainingRows.push(rawData[a]);
+                }
+            }
+            for (var a in pertainingRows) {
+                teacherNames.push(pertainingRows[a][7])
+            }
+            removeDuplicates(teacherNames);
+            for (var a in teacherNames) {
+                teacherBlocks.push([]);
+                for (var b in pertainingRows) {
+                    if (pertainingRows[b][7] == teacherNames[a]) {
+                        teacherBlocks[a].push(pertainingRows[b][5]);
+                    }
+                }
+            }
+            let result = new Course(currentClass, parseInt(pertainingRows[0][0],10), teacherNames, teacherBlocks);
+            for (var a = rawData.length - 1; a >= 0; a--) {
+                if (rawData[a][2] == currentClass) {
+                    rawData.splice(a, 1);
+                }
+            }
+            currentClass = "none";
+        }
+    }
+    console.log(allCourses);
+}
+
+function removeDuplicates(arr) {
+    for (var a = 0; a < arr.length; a++) {
+        for (var b = arr.length - a; b >= 1; b--) {
+            if (arr[a] == arr[a + b]) {
+                arr.splice(a + b, 1);
+            }
+        }
+    }
+}
+
+/*let precalcH = new Course("Precalc Honors", 1, ["Li", "Gribler"], [[2, 6, 8], [3, 7]]);
+let latin3 = new Course("Latin 3", 6, ["TCHR XY"], [[2]]);
+let apPhys2 = new Course("AP Physics 2", 2, ["Cooley"], [[5, 7]]);
+let apComp = new Course("AP Comp Sci", 1, ["Simon"], [[1, 3, 6, 7]]);
+let ush = new Course("US History", 4, ["Sloan", "Furey", "Starr", "Staff B", "Prophet", "Worth"], [[1, 3], [2, 4, 8], [2, 4, 6], [3, 6, 8], [5, 7], [5, 7]]);
+let apEng7577 = new Course("AP English 75/77", 3, ["Moffit"], [[2, 4]]);
+let chemH = new Course("Chemistry A Honors", 2, ["Fong", "Trimble"], [[1], [2, 3, 4, 6]]);
+let advDrama = new Course("Advanced Drama", 5, ["Ullman"], [[5, 6]]);
+let yoga = new Course("Yoga", 7, ["Doherty", "Staff A"], [[3], [6]]);
+let health = new Course("Health", 8, ["Cole", "Brooks"], [[2, 3, 4, 6, 8], [3, 5, 7]]);*/
+
+/*function tabletopInit(){
+    var announcer = Tabletop.init({
+        key: "https://docs.google.com/spreadsheets/d/1v85ZUS42eJ2fh_kCk3VGzlp3HBWwdvN5C6hYfg3WvWE/edit?usp=sharing",
+        callback: showInfo,
+        simpleSheet: true
+    })
+}
+function showInfo(data, tabletop) {
+    alert('Successfully processed!')
+    document.getElementById("tester").innerHTML = data;
+}
+tabletopInit();
+
+var rawData = announcer.toArray();*/
+//document.getElementById("tester").innerHTML = rawData.length;
+
+//window.addEventListener('DOMContentLoaded', tabletopInit)
+
+function inArray(obj, arr) {
+    for (var a in arr) {
+        if (arr[a] == obj) {
+            return true;
+        }
+    }
+    return false;
+}
